640978a94734210104cf26ae5e0a98c6f106f63d
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-07-19 21:41:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "20919625"))
+    API_HASH = os.environ.get("API_HASH", "40168846bf06f4ff443f0f7a4182bf8d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    MONGO_URI = os.environ.get("MONGO_URI", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://jigam96466:YpvmFQpmb9y9gzcU@cluster0.wajbn6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6233910543"))
     
     # Default caption format
     DEFAULT_CAPTION = """{filename}
